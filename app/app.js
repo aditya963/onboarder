e9460e484b1e0608945a81cf6c1fb839fd8a5ea5
Aditya Subramanyam <aditya.subramanyam@practo.com>
2015-12-18 15:20:36 +0530

    fixed question file

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import controller from './slack.js';
 import convoManager from './managers/convoManager.js';
-// give the bot something to listen for.
+
 controller.hears('hello', 'direct_message,direct_mention,mention', function (bot, message) {
     bot.reply(message, 'Hello yourself.');
 });
